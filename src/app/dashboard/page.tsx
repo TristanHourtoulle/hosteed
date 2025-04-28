a28b60b724e16b4c55f8b0bc67710eb2380276af
Pierre Maurer <91548935+PierreMaurer@users.noreply.github.com>
2025-04-28 15:06:07 -0700

    ✨add authentication with NextAuth and Google provider, including session management and user model updates

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useSession} from "next-auth/react";
+import {signOut} from "next-auth/react";
+import Link from "next/link";
+
+
+export default function Dashboard () {
+    const {data: session} = useSession();
+
+    return (
+        <>
+            {session?.user ? (
+                <div>
+                    <p>Welcome {session.user.name}</p>
+                    <button onClick={() => signOut()}>Sign out</button>
+                </div>
+            ) : (
+                <Link href={"/login"}>
+                    <button>Click here to sign In</button>
+                </Link>
+            )}
+        </>
+    );
+};
